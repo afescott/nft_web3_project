ca6db9e903f165922f671cc266ae2f7dfe762897
ashley.escott@hotmail.com <ashley.escott@hotmail.com>
2025-02-22 03:18:52 +0000

    feat: base voting + test

--- tests/voting_de_app.ts
+++ tests/voting_de_app.ts
@@ -6,13 +6,13 @@ import { startAnchor } from 'solana-bankrun';
 import { VotingDeApp } from '../target/types/voting_de_app';
 import { Program } from '@coral-xyz/anchor';
 
-const IDL = require('../target/idl/voting.json')
+const IDL = require('../target/idl/voting_de_app.json')
 
-const VOTING_ADDRESS = new PublicKey("5s3PtT8kLYCv1WEp6dSh3T7EuF35Z6jSu5Cvx4hWG79H");
+const VOTING_ADDRESS = new PublicKey("FJaBgneiubwaV9xCvqxejWd1gxjvm1Vm2Xxbz16rgBz4");
 
 describe('voting_de_app', async () => {
   it('Initialize poll', async () => {
-  const context = await startAnchor("", [{name : "voting", programId: VOTING_ADDRESS} ], []);
+  const context = await startAnchor("", [{name : "voting_de_app", programId: VOTING_ADDRESS} ], []);
   const provider = new BankrunProvider(context);
 
   
@@ -23,8 +23,7 @@ describe('voting_de_app', async () => {
 
 await votingProgram.methods.initializePoll(
   //for u64 types. 1 as u64
-  new anchor.BN(1),
-
+   new anchor.BN(1),
    new anchor.BN(0),
    new anchor.BN(1839556817),
    "What's your fav peanut butter?",
